b086ac89d4b62a0704c3f527e3e514ef4d81072b
yxpjseo <yjins2004@gmail.com>
2026-09-11 23:40:45 +0900

    Initial commit

---
+++ Assets/S03_Mesh2D/S03_SingleTriangleMesh_Finish.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_SingleTriangleMesh_Finish : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 1f, 0f),      // 0
+            new Vector3(1f, 0.3f, 0f),    // 1
+            new Vector3(0.6f, -1f, 0f),   // 2
+            new Vector3(-0.6f, -1f, 0f),  // 3
+            new Vector3(-1f, 0.3f, 0f),   // 4
+        };
+
+        int[] triangles = new int[] 
+        { 
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4 
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
